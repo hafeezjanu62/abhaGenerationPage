f734226b99ee339c0c2520c9f8ce40d6c24afe98
Abdul-Hafeez-Shaik <abdulhafeez43949@gmail.com>
2023-04-05 12:23:27 +0530

    implemented Copy Id, Aadhar screen and Validations

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,6 +1,7 @@
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { useState, useEffect } from "react";
+import { useCopyToClipboard } from 'usehooks-ts'
 import axios from "axios";
 import "../Styles/HomePage.css";
 import { ColorRing } from "react-loader-spinner";
@@ -16,13 +17,18 @@ import 'react-dropdown/style.css';
 function HomePage() {
   const [value, setValue] = useState();
   const [page, setPage] = useState(1);
-  const [loader, setLoader] = useState(true);
+  const [loader, setLoader] = useState(false);
   const [accept, setAccept] = useState(false);
   const [data, setData] = useState();
   const [txnId, setTxnId] = useState();
   const [dob, setDob] = useState(new Date());
   const [otpResponse,setOtpResponse] = useState();
-const [name,setName] = useState('');
+  const [name,setName] = useState('');
+  const [aadharNumber, setAadharNumber] = useState();
+  const [aadharError, setAadharError] = useState(false);
+  const [aadharMessage, setAadharMessage] = useState(false);
+  const [copyText, setCopyText] = useCopyToClipboard()
+  const [nameObj, setNameObj] = useState({name1: 'vaishak8722', id1: '@abdm'})
   const options = [
     'Male', 'Female', 'Others'
   ];
@@ -52,6 +58,7 @@ const [name,setName] = useState('');
   };
 
   const mobileOtpGenerationApi = () => {
+    debugger
     var token = data.sessionData.accessToken;
     axios
       .post(
@@ -199,6 +206,7 @@ const createAbhaIdButton = () => {
     setPage(2);
   };
   const sendOtpClicked = (item) => {
+    debugger
     if (accept) mobileOtpGenerationApi();
   };
   const otpVerifyClicked = () => {
@@ -207,8 +215,36 @@ const createAbhaIdButton = () => {
   const continueClicked = () => {
     setPage(5);
   };
-  
 
+
+  const aadharClicked = () => {
+    setPage(6);
+  };
+  const onChangeAadhar = (e) => {
+    setAadharNumber(e.target.value)
+  }
+  const aadharValidation = (e) => {
+    let aadhar = e.target.value
+    var adhartwelvedigit = /^\d{12}$/;
+    var adharsixteendigit = /^\d{16}$/;
+    if (aadhar !== '') {
+        if ((aadhar.match(adhartwelvedigit)) || (aadhar.match(adharsixteendigit))) {
+            setAadharError(false)
+            setAadharMessage("")
+        }
+        else {
+            let message = "Enter valid aadhar..";
+            setAadharError(true)
+            setAadharMessage(message)
+        }
+    }
+  }
+  const copyId = () => {
+    setCopyText(`${nameObj.name1} ${nameObj.id1}`)
+  }
+
+
+  
 
   const otpHandler = (e) => {
     otp = e.target.value;
@@ -255,8 +291,8 @@ const createAbhaIdButton = () => {
                   <div className="formContent-button" onClick={mobileClicked}>
                     <p className="formContent-button-text">Mobile</p>
                   </div>
-                  <div className="formContent-button">
-                    <p className="formContent-button-text">AAdhar</p>
+                  <div className="formContent-button" onClick={aadharClicked}>
+                    <p className="formContent-button-text">Aadhar</p>
                   </div>
                 </div>
               )}
@@ -355,6 +391,67 @@ const createAbhaIdButton = () => {
                   >
                     <p className="mobileNumber-button-text">Continue</p>
                   </div>
+                </div>
+              )}
+
+              {page === 6 && (
+                <div className="details">
+                <p className="abha_id_heading">
+                  ABHA ID Created Successfully
+                </p>
+                <div> 
+                  <p className="your_abha_id">Your ABHA ID</p>
+                </div>
+
+                <p className="copyIdText" id="copy_id">{nameObj.name1}<span className="copyIdSpan"> {nameObj.id1} </span></p>
+                
+                <div
+                  className="mobileNumber-button"
+                  onClick={copyId}
+                >
+                  <p className="mobileNumber-button-text"  >Copy ID</p>
+                </div>
+                <p className="copyIdSpan">Copied value: {copyText ?? 'Nothing is copied yet!'}</p>
+              </div>
+              )}
+              
+
+              {page === 7 && (
+                <div className="mobileNumber">
+                  <p className="formContent-heading">
+                    Create ABHA using Mobile
+                  </p>
+                  <p className="mobileNumber-text">Aadhar number</p>
+
+                  <div> 
+                    {/* <input 
+                      className="aadharInput" 
+                      type="text"
+                      pattern="[0-9]"
+                      maxLength="12"
+                      value={aadharNumber} 
+                      onChange={onChangeAadhar}
+                      placeholder="Enter Aadhar number"
+                      
+                    /> */}
+                    <input type="text" placeholder="Enter Aadhar No" name="aadharNo" maxLength={12}
+                      value={aadharNumber} onBlur={aadharValidation} onChange={onChangeAadhar}
+                      className="aadharInput" />
+                      {aadharError &&
+                      <div className="text-error" >
+                          {aadharMessage}
+                      </div>}
+                  </div>
+
+                  <input
+                    className="checkboxAgreed"
+                    type="checkbox"
+                    value="accept"
+                    onChange={handleCheckBoxClicked}
+                  />
+                  <label className="checkboxText" htmlFor="vehicle1">
+                    I agree to Terms of use & Privacy policy
+                  </label>
                 </div>
               )}
             </div>
